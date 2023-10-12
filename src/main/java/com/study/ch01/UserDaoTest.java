23fd3b66c1bb658a738eff5a50efb07bab8ffce0
yangrangrang <gkssk2309@gmail.com>
2023-10-13 08:16:44 +0900

    pom

--- src/main/java/com/study/ch01/UserDaoTest.java
+++ src/main/java/com/study/ch01/UserDaoTest.java
@@ -24,7 +24,7 @@ public class UserDaoTest {
         UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
-        user.setId("test107");
+        user.setId("test108");
         user.setName("yanghanna");
         user.setPassword("1234");
 
