d02b7c682581046d303c9996d4d1d91b67add8fa
yangrangrang <gkssk2309@gmail.com>
2023-10-13 23:18:37 +0900

    1-13

--- src/main/java/com/review/ch01/Test.java
+++ src/main/java/com/review/ch01/Test.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class Test {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
+        ConnectionMaker connectionMaker = new NConnectionMaker();
 
         UserDao dao = new UserDao(connectionMaker);
 
