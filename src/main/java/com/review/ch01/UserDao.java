6c8a5464b8550bb291cdf9f9ded05827ba90096a
yangrangrang <gkssk2309@gmail.com>
2023-10-13 23:09:24 +0900

    1-6

--- src/main/java/com/review/ch01/UserDao.java
+++ src/main/java/com/review/ch01/UserDao.java
@@ -5,8 +5,14 @@ import com.review.ch01.domain.User;
 import java.sql.*;
 
 public abstract class UserDao {
+
+    private SimpleConnectionMaker simpleConnectionMaker;
+
+    public UserDao() {
+        simpleConnectionMaker = new SimpleConnectionMaker();
+    }
     public void add (User user) throws ClassNotFoundException, SQLException {
-        Connection c = getConnection();
+        Connection c = simpleConnectionMaker.makeNewConnection();
 
         PreparedStatement ps = c.prepareStatement("insert into users(id,name,password) values (?,?,?)");
         ps.setString(1, user.getId());
@@ -20,7 +26,7 @@ public abstract class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = getConnection();
+        Connection c = simpleConnectionMaker.makeNewConnection();
 
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
         ps.setString(1, id);
@@ -41,7 +47,7 @@ public abstract class UserDao {
     }
 
     public void delete(User user) throws ClassNotFoundException, SQLException {
-        Connection c = getConnection();
+        Connection c = simpleConnectionMaker.makeNewConnection();
 
         PreparedStatement ps = c.prepareStatement(
                 "delete from users where id = ?");
