53b550737528402642c40a70a5f82b9a44c3ad79
yangrangrang <gkssk2309@gmail.com>
2023-10-13 22:59:33 +0900

    1-3

---
+++ src/main/java/com/review/ch01/Test.java
@@ -0,0 +1,29 @@
+package com.review.ch01;
+
+import com.review.ch01.domain.User;
+
+import java.sql.SQLException;
+
+public class Test {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("userTest");
+        user.setName("yang");
+        user.setPassword("yang");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + "등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + "조회 성공");
+
+        dao.delete(user2);
+        System.out.println("DB 삭제");
+    }
+}
