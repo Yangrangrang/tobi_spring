334c5553adf56c59de83d839edd1ddcebd5dc936
yangrangrang <gkssk2309@gmail.com>
2023-10-15 23:46:35 +0900

    1-48

--- src/main/java/com/review/ch01/DaoFactory.java
+++ src/main/java/com/review/ch01/DaoFactory.java
@@ -2,6 +2,9 @@ package com.review.ch01;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DaoFactory {
@@ -10,6 +13,18 @@ public class DaoFactory {
         UserDao userDao = new UserDao();
         userDao.setDataSource(dataSource());
         return userDao;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
+        dataSource.setUrl("jdbc:mysql://localhost:3306/SPRINGSTUDYDB");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+
+        return dataSource;
     }
 
     /*
