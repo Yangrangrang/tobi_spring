b7328be57dce3d4c84d1bc4686de9519be531d56
yangrangrang <gkssk2309@gmail.com>
2023-10-22 21:07:41 +0900

    2-1

---
+++ src/main/java/com/study/ch02/UserDaoTest.java
@@ -0,0 +1,33 @@
+package com.study.ch02;
+
+import com.study.ch01.User;
+import com.study.ch01.UserDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.sql.SQLException;
+
+public class UserDaoTest {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        User user = new User();
+        user.setId("test108");
+        user.setName("yanghanna");
+        user.setPassword("1234");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + "등록");
+
+        User user2 = dao.get(user.getId());
+
+        System.out.println(user2.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + "조회");
+    }
+}
