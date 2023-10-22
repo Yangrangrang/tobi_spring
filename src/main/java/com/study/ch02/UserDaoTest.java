b21a1db9c7b8f74e3c08143b441bae53901bb2ff
yangrangrang <gkssk2309@gmail.com>
2023-10-22 22:07:35 +0900

    2-2

--- src/main/java/com/study/ch02/UserDaoTest.java
+++ src/main/java/com/study/ch02/UserDaoTest.java
@@ -24,7 +24,6 @@ public class UserDaoTest {
 
         User user2 = dao.get(user.getId());
 
-        System.out.println(user2.getId());
         System.out.println(user2.getName());
         System.out.println(user2.getPassword());
 
