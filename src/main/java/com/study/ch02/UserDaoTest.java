992eb5e3e9dccaa9bf084a226ec28c54ee98724d
yangrangrang <gkssk2309@gmail.com>
2023-10-28 23:54:23 +0900

    2-4

--- src/main/java/com/study/ch02/UserDaoTest.java
+++ src/main/java/com/study/ch02/UserDaoTest.java
@@ -2,32 +2,59 @@ package com.study.ch02;
 
 import com.study.ch01.User;
 import com.study.ch01.UserDao;
+import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
+
 public class UserDaoTest {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+    //    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+//        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+//
+//        UserDao dao = context.getBean("userDao", UserDao.class);
+//
+//        User user = new User();
+//        user.setId("testtest01");
+//        user.setName("yanghanna");
+//        user.setPassword("1234");
+//
+//        dao.add(user);
+//
+//        System.out.println(user.getId() + "등록");
+//
+//        User user2 = dao.get(user.getId());
+//
+//        System.out.println(user2.getName());
+//        System.out.println(user2.getPassword());
+//
+//        System.out.println(user2.getId() + "조회");
+//
+//        if(!user.getName().equals(user2.getName())){
+//            System.out.println("테스트 실패 (name)");
+//        } else if (!user.getPassword().equals(user2.getPassword())) {
+//            System.out.println("테스트 실패 (password)");
+//        } else {
+//            System.out.println("조회 테스트 성공");
+//        }
+//
+//    }
+    @Test
+    public void addAndGet() throws SQLException, ClassNotFoundException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
-        user.setId("testtest01");
+        user.setId("t1");
         user.setName("yanghanna");
         user.setPassword("1234");
 
         dao.add(user);
 
-        System.out.println(user.getId() + "등록");
-
         User user2 = dao.get(user.getId());
-
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
-
-        System.out.println(user2.getId() + "조회");
 
         if(!user.getName().equals(user2.getName())){
             System.out.println("테스트 실패 (name)");
@@ -36,6 +63,6 @@ public class UserDaoTest {
         } else {
             System.out.println("조회 테스트 성공");
         }
-
     }
+
 }
