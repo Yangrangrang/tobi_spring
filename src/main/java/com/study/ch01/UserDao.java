5b8f92507a1c10bbef8f525a9fa9a2a87e3e7b25
yangrangrang <gkssk2309@gmail.com>
2023-10-30 17:29:02 +0900

    2-7

--- src/main/java/com/study/ch01/UserDao.java
+++ src/main/java/com/study/ch01/UserDao.java
@@ -111,6 +111,17 @@ public class UserDao {
 //        return this.user;
     }
 
+    public void deleteAll() throws SQLException {
+        Connection c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement("delete from users");
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
     // connection 메소드
 //    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
 
