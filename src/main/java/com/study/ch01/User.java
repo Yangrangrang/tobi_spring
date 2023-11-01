f923167e2469fc9a3eaccb5afe9de1b70b1ae3bc
yangrangrang <gkssk2309@gmail.com>
2023-11-01 11:16:10 +0900

    2-10

--- src/main/java/com/study/ch01/User.java
+++ src/main/java/com/study/ch01/User.java
@@ -6,6 +6,15 @@ public class User {
     private String name;
     private String password;
 
+    public User(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
     public void setId(String id){this.id = id;}
     public String getId(){return id;}
     public void setName(String name){this.name = name;}
