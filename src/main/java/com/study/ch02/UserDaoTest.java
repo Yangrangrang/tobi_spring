48472b6115f599b3701b9140204427af50f44ce8
yangrangrang <gkssk2309@gmail.com>
2023-11-01 17:23:29 +0900

    2-11

--- src/main/java/com/study/ch02/UserDaoTest.java
+++ src/main/java/com/study/ch02/UserDaoTest.java
@@ -5,6 +5,7 @@ import com.study.ch01.UserDao;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
@@ -64,6 +65,29 @@ public class UserDaoTest {
 
         assertThat(user2.getName()).isEqualTo(user.getName());
         assertThat(user2.getPassword()).isEqualTo(user.getPassword());
+    }
+
+    @Test
+    public void count() throws SQLException, ClassNotFoundException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        User user1 = new User("test1", "test1", "test1");
+        User user2 = new User("test2", "test2", "test2");
+        User user3 = new User("test3", "test3", "test3");
+
+        dao.deleteAll();
+        assertThat(dao.getCount()).isSameAs(0);
+
+        dao.add(user1);
+        assertThat(dao.getCount()).isSameAs(1);
+
+        dao.add(user2);
+        assertThat(dao.getCount()).isSameAs(2);
+
+        dao.add(user3);
+        assertThat(dao.getCount()).isSameAs(3);
+
     }
 
     public static void main(String[] args) {
