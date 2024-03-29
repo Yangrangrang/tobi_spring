88740fe036b94a6a0bd61a5d4700eaac2aca95b1
eYanghanna <yhnn2131@emoney.co.kr>
2024-03-29 10:16:21 +0900

    Test

--- src/main/java/com/review/test/Test.java
+++ src/main/java/com/review/test/Test.java
@@ -1,4 +1,12 @@
 package com.review.test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Test {
+    private static final Logger log = LoggerFactory.getLogger(Test.class);
+
+    public void Test() {
+        log.info("Test");
+    }
 }
